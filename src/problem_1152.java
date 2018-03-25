c93c4f4de9f106edf55e0e32b0e1c5be4f3420a4
SeungLee <dltmd12365@gmail.com>
2018-03-25 21:06:47 +0900

    problem 1152 (문제가 짜증났다... 분명 공백 2개 나오는 거 안나오고 모든 빈 칸에 대한 해결책도 없었는데 결국은 다 처리해줘야 했어 ㅠㅠ)

--- src/problem_1152.java
+++ src/problem_1152.java
@@ -7,10 +7,17 @@ public class problem_1152 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String sentence = s.nextLine();
+        int count = 0;
 
-        String[] words = sentence.split(" ");
+        sentence = sentence.trim();
+        for(int i = 0; i < sentence.length(); i++){
+            if(sentence.charAt(i) == ' ' && sentence.charAt(i-1) != ' '){
+                count++;
+            }
+        }
+        if(sentence.equals("")) System.out.print(0);
+        else System.out.print(count+1);
 
-        System.out.print(words.length);
     }
 
 
