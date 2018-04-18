7b7876eb1ddf7ba894ccb1ddc2e0cbe54c28ef6c
SeungLee <ls0408@naver.com>
2018-04-18 20:04:07 +0900

    problem_2775
    https://www.acmicpc.net/problem/2775

--- src/problem_2775.java
+++ src/problem_2775.java
@@ -9,34 +9,25 @@ public class problem_2775 {
 
         int num = s.nextInt();
         int k, n;
-        int[] twoUnderFloor;
-        int[] underFloor;
+        int[][] floors = new int[15][15];
         int[] answer = new int[num];
 
-        while(num > 0) {
+        for(int i = 0; i < floors.length; i++){
+            floors[i][0] = 1;
+            floors[0][i] = i+1;
+        }
+        for(int i = 1; i < floors.length; i++){
+            for(int j = 1; j < floors[i].length; j++){
+                floors[i][j] = floors[i-1][j] + floors[i][j-1];
+            }
+        }
+        for(int i = 0; i < num; i++){
             k = s.nextInt();
             n = s.nextInt();
-            twoUnderFloor = new int[n];
-            underFloor = new int[n];
-
-            for (int i = 0; i < twoUnderFloor.length; i++) {
-                twoUnderFloor[i] = i + 1;
-            }
-            for (int i = 0; i < k; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (j == 0) underFloor[j] = 1;
-                    else {
-                        underFloor[j] = underFloor[j - 1] + twoUnderFloor[j];
-                    }
-                    twoUnderFloor[j] = underFloor[j];
-                }
-            }
-
-            answer[answer.length - num] = underFloor[n-1];
-            num--;
+            answer[i] = floors[k][n-1];
         }
-        for(int i = 0; i < answer.length; i++){
-            System.out.println(answer[i]);
+        for(int result: answer){
+            System.out.println(result);
         }
     }
 }
