bbc7b0d4ea0ce26c1e7e178f6345a70f97af7846
SeungLee <ls0408@naver.com>
2020-01-05 23:12:07 +0900

    add problem_10817.js

---
+++ src/javascript/baekjoon/2step/problem_10817.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on('line', function(answer) {
+	let numbers = answer.trim().split(' ').map(el => parseInt(el));
+
+    numbers.sort((a, b) => a < b);
+    
+    console.log(numbers[1]);
+    rl.close();
+});
